db91d52eca4c6979229c60cc34c2270ccf4977b5
MonoRandon <162451933+MonoRandon@users.noreply.github.com>
2024-03-26 10:47:46 -0300

    Add files via upload

---
+++ scrips.js
@@ -0,0 +1,36 @@
+//Declarando las variables String
+
+//String
+let bienvenida = "Wena cabros del whatsapp"
+let presentacion = "Hola, soy Brandon Morales, conocido como alias el Mono"
+
+
+let nombre = "Brandon"
+let apellido = "Morales"
+let usuario = "RandonconMorales"
+
+//Integer
+
+let edad = 16
+let altura = 1.66
+let peso = 67.45
+
+//Boleam
+
+let mono = true
+let adulto = false
+let chambeador = true
+let gey = false
+
+//Arreglo
+
+let zaza = [1,2,3,4,5]
+let datos = [16,mono,zaza,cerru]
+
+//Objeto
+
+let Usuario = {nombre:"Brandon", apellido:"Morales", edad: 16, altura: 1.66, peso: 67.45}
+let objeto = {circulo:"esfera", trianfulo:"piramide", cuadrado:"cubo"}
+
+console.log("Hola mundo")
+console.log("zaza")
